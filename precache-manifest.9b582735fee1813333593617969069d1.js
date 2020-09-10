b876db08b5326d282f4d234f236dc4121292abbe
AndrewShapoval <andrewshapoval88@gmail.com>
2020-09-10 17:42:29 +0300

    Updates

---
+++ precache-manifest.9b582735fee1813333593617969069d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d7ec78344d206989d7b296e025ecef",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "873b21a1c23fa79cc783",
+    "url": "/Portfolio/static/css/main.0fe770cd.chunk.css"
+  },
+  {
+    "revision": "47983045cbfa7ce282c5",
+    "url": "/Portfolio/static/js/2.1d701f70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.1d701f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "873b21a1c23fa79cc783",
+    "url": "/Portfolio/static/js/main.21d96f52.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
