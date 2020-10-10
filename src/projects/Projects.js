de1032f5247d2de9d443b82e109aa8f04c344a77
AndrewShapoval <andrewshapoval88@gmail.com>
2020-10-10 13:58:44 +0300

    add SCSS

---
+++ src/projects/Projects.js
@@ -0,0 +1,32 @@
+import React from "react";
+import style from "./Projects.module.scss"
+import styleContainer from "../common/styles/Container.module.css";
+import {Project} from "./project/Project";
+import {Title} from "../common/components/title/Title";
+import socialImage from "./../assets/image/social.jpg"
+import todoImage from "./../assets/image/todolist.jpg"
+
+export const Projects = () => {
+
+    const social = {
+        backgroundImage: `url(${socialImage})`,
+    };
+
+    const todolist = {
+        backgroundImage: `url(${todoImage})`,
+    };
+
+    return (
+        <div className={style.projectsBlock}>
+            <div className={`${styleContainer.container} ${style.projectsContainer}`}>
+                <Title text={"Projects"}/>
+                <div className={style.projects}>
+                    <Project style={social} title={'Social network'}
+                             description={'kerknnglrw lwelnglrngn wlgnwlkgn wlkegnwelkgn'}/>
+                    <Project style={todolist} title={'Todolist'}
+                             description={'jwbv wenfnfrjng jgjnrjg ergnrjkg rg erngnr egnegn'}/>
+                </div>
+            </div>
+        </div>
+    )
+}
