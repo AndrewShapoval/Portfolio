110b9997d97c999d479376b737e294df55d864a7
AndrewShapoval <andrewshapoval88@gmail.com>
2020-10-26 22:32:16 +0300

    Updates

---
+++ precache-manifest.3f45cf0271633dbfe727a9e7c3491c32.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ee31ce0e9a4cb332457bd72efd6918",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1dc96e6553cc37072386",
+    "url": "/Portfolio/static/css/main.6821d19a.chunk.css"
+  },
+  {
+    "revision": "d770809114156e3cd033",
+    "url": "/Portfolio/static/js/2.e719be0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.e719be0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc96e6553cc37072386",
+    "url": "/Portfolio/static/js/main.8a0acf86.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "21d620ef0ececdc58f216f04f20d915b",
+    "url": "/Portfolio/static/media/github.21d620ef.svg"
+  },
+  {
+    "revision": "0b000652cc1576118f8822cb19a6bafd",
+    "url": "/Portfolio/static/media/linkedin.0b000652.svg"
+  },
+  {
+    "revision": "ddf75e13b3978293cc3e4e92cf0241e3",
+    "url": "/Portfolio/static/media/social.ddf75e13.jpg"
+  },
+  {
+    "revision": "a451f456eac79406086651dc449c331a",
+    "url": "/Portfolio/static/media/telegram.a451f456.svg"
+  },
+  {
+    "revision": "7dd9125583dd5477bc411391e8252d6b",
+    "url": "/Portfolio/static/media/todolist.7dd91255.jpg"
+  },
+  {
+    "revision": "b03024c4d635b0a304e483e1d9fa34d8",
+    "url": "/Portfolio/static/media/vk.b03024c4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.53c017a939d1fb410e9e4e879207008e.js"
+  "/Portfolio/precache-manifest.3f45cf0271633dbfe727a9e7c3491c32.js"
 );
 
 self.addEventListener('message', (event) => {
